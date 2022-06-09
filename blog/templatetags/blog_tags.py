ae05ff4efe0da612bd3bf196f1178f3c5eb256b5
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-09 16:25:02 +0430

    Add template tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from blog.models import Post, Category
+
+register = template.Library()
+
+@register.inclusion_tag('blog/blog-posts.html')
+def latestpost():
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
+    return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-categories.html')
+def postcategory():
+    posts = Post.objects.filter(status=1)
+    catgories = Category.objects.all()
+    cat_dict = {}
+    for name in catgories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'category':cat_dict}
