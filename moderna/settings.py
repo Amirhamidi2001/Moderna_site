85e1449b73fdee71f26aa2479fcc148dca71e1cf
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-13 12:40:10 +0430

    Update settings.py

--- moderna/settings.py
+++ moderna/settings.py
@@ -39,10 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'django_extensions',
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
     'accounts.apps.AccountsConfig',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
