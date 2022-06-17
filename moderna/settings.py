ffe61a2a4a72f87b841f0527e907856ea642340e
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-17 18:17:56 +0430

    Update project

--- moderna/settings.py
+++ moderna/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-django_heroku.settings(locals())
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR / 'assets')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
@@ -139,3 +136,5 @@ MEDIA_ROOT = 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_heroku
+django_heroku.settings(locals())
